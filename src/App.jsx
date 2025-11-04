fc3dd40691ec8425c586faae81e4be6ea9c16eca
dest411 <vadimstrizhak652@gmail.com>
2025-11-04 20:52:42 +0200

    welcome text

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './index.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import WelcomeSection from './components/WelcomeSection'
 
 const App = () => {
   return (
     <div className='w-full h-auto flex flex-col justify-center items-center' >
       <Header/>
       <Hero/>
+      <WelcomeSection/>
       
     </div>
     
---
+++ src/components/WelcomeSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const WelcomeSection = () => {
+  return (
+    <div className='text-4xl relative w-[95%] h-auto flex flex-col items-center' >
+        <h1 className='font-[Caveat] text-9xl opacity-10 absolute mt-14'>Welcome to Naples</h1>
+        <h2 className='text-8xl mt-30 opacity-75' >Welcome to Naples</h2>
+        <p className='text-center opacity-60  text-5xl' >We started working in 2007 and in 2012 we were able to <br /> open our second branch and now we have several <br /> branches all over the country.</p>
+    </div>
+  )
+}
+
+export default WelcomeSection
