41c8560d851ea4a9b1f79b3e67ddac6fc8c8202a
dest411 <vadimstrizhak652@gmail.com>
2025-11-04 19:27:17 +0200

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
+import arrow from '../png/arrow.svg'
+import dandruff from '../png/dandruff.svg'
 
 const Header = () => {
   return (
-    <div className='flex bg-amber-400 w-[95%] h-auto justify-between ' >
-        <div className='flex gap-25' >
-            <p>LOGO</p>
+    <div className='flex w-[95%] h-[60px] justify-between items-center' >
+        <div className='flex gap-25 items-center ' >
+            <p className=' text-4xl ' >Naples</p>
             <nav>
-                <ul className='flex gap-10' >
+                <ul className='flex gap-10 text-3xl' >
                     <li>Home</li>
-                    <li>Menu</li>
+                    <li className='flex gap-1.5 relative' >Menu <img src={arrow} className='absolute right-[-17px] top-[40%]' alt="" /> </li>
                     <li>Contact Us</li>
                 </ul>
             </nav>
         </div>
         
-        <input className='border' type="text" placeholder='Search something...' />
+        <div type className='flex justify-center items-center border w-[300px] h-[50px] py-1 bg-black/6 border-black/10 rounded-2xl'>
+            <input className='w-[300px] h-[50px]' type="text" placeholder='Search something...' />
+            <img className='w-4 h-4' src={dandruff} alt="" />
+        </div>
     </div>
   )
 }
