92768cc5fd0947269f533315ef1729e910eadc7b
dest411 <vadimstrizhak652@gmail.com>
2025-11-05 02:53:39 +0200

    menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -34,7 +34,7 @@ const Menu = () => {
   return (
     <div className='bg-red-1 w-[95%] h-auto my-20 '>
         
-        <div className='flex gap-15 w-full h-[120px]'>
+        <div className='flex gap-15 w-full h-[120px] gap-48'>
             <div
                 className="absolute  left-0 rounded-snm bg-red-700   text-white  text-6xl 
                     font-semibold   px-6  py-6   w-60 text-center flex items-center justify-center
@@ -46,12 +46,12 @@ const Menu = () => {
 
             <div className='w-[220px] h-[120px] bg-blue-950 '  ></div>
 
-            <div className=' w-full flex justify-between items-center '>
+            <div className=' w-full flex gap-16 items-center '>
 
                 {Foods.map(food => {
                     return (
                         <div className='flex items-center gap-4 border border-black/7 rounded-2xl box-shadow: -1px 0px 80px -3px rgba(0,0,0,0.75)
-                    shadow-[-1px_0px_80px_-3px_rgba(0,0,0,0.20)] p-3' key={food.id}>
+                             shadow-[0px_0px_20px_-3px_rgba(0,0,0,0.40)] py-5 px-8 cursor-pointer' key={food.id}>
                             <img className='w-[100px] h-[80px]' src={food.photo} alt="" />
                             <span className='w-[1px] h-[80px] bg-black/10 ' ></span>
                             <p className='text-5xl' >{food.name}</p>
