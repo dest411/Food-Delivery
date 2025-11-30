6353128f7cfb53bfd8c2f1e2a6063f0970ad6788
dest411 <vadimstrizhak652@gmail.com>
2025-11-30 06:10:27 +0200

    css not loaded remainded food

--- src/Food.jsx
+++ src/Food.jsx
@@ -2,12 +2,9 @@ import pizza from './png/pizza.svg'
 import pasta from './png/pasta.svg'
 import burger from './png/burger.svg'
 
-// Pizza Photos (залишив ваші локальні імпорти для піци)
 import PepperoniPizza from './png/PizzaPhoto/PepperoniPizza.svg'
 import VegetablesPizza from './png/PizzaPhoto/VegetablesPizza.svg'
 import MargheritaPizza from './png/PizzaPhoto/MargheritaPizza.svg'
-
-import ChickenBurger from './png/BurgersPhoto/CheeseBurger.png'
 
 const Foods = [
     {
@@ -41,22 +38,19 @@ const Foods = [
         photo: burger,
         typeFood: {
             Cheese: {
-                // Онлайн посилання
-                typePhoto: ChickenBurger, 
+                typePhoto: null, 
                 name: 'Cheese Burger',
                 price: '45,00',
                 ingredients: ['Beef patty', 'Cheddar cheese', 'Pickles', 'Onion', 'Mustard', 'Ketchup']
             },
             Chicken: {
-                // Онлайн посилання
-                typePhoto: ChickenBurger, 
+                typePhoto: null, 
                 name: 'Chicken Burger',
                 price: '42,00',
                 ingredients: ['Chicken fillet', 'Lettuce', 'Tomato', 'Mayonnaise', 'Sesame bun']
             },
             Double: {
-                // Онлайн посилання
-                typePhoto: ChickenBurger,
+                typePhoto: null,
                 name: 'Double Beef Burger',
                 price: '65,00',
                 ingredients: ['Double beef patty', 'Bacon', 'BBQ sauce', 'Caramelized onion', 'Cheese']
@@ -69,22 +63,19 @@ const Foods = [
         photo: pasta,
         typeFood: {
             Carbonara: {
-                // Онлайн посилання
-                typePhoto: 'https://images.unsplash.com/photo-1612874742237-6526221588e3?auto=format&fit=crop&w=500&q=60', 
+                typePhoto: null, 
                 name: 'Pasta Carbonara',
                 price: '52,00',
                 ingredients: ['Spaghetti', 'Bacon', 'Egg yolk', 'Parmesan cheese', 'Black pepper']
             },
             Bolognese: {
-                // Онлайн посилання
-                typePhoto: 'https://images.unsplash.com/photo-1626844131082-256783844137?auto=format&fit=crop&w=500&q=60', 
+                typePhoto: null, 
                 name: 'Pasta Bolognese',
                 price: '48,00',
                 ingredients: ['Tagliatelle', 'Minced beef', 'Tomato sauce', 'Carrot', 'Celery', 'Red wine']
             },
             Pesto: {
-                // Онлайн посилання
-                typePhoto: 'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?auto=format&fit=crop&w=500&q=60',
+                typePhoto: null,
                 name: 'Pasta Pesto',
                 price: '46,00',
                 ingredients: ['Penne', 'Basil pesto', 'Pine nuts', 'Olive oil', 'Parmesan cheese']
