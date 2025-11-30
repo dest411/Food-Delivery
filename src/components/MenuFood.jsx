2aa5710258767dad0f9b12bf8e5e130dd475e5ca
dest411 <vadimstrizhak652@gmail.com>
2025-11-30 05:28:40 +0200

    btn in css

--- src/components/MenuFood.jsx
+++ src/components/MenuFood.jsx
@@ -14,23 +14,25 @@ const MenuFood = ({activeMenu}) => {
   return (
     <div className='w-full max-w-[1500px] mx-auto ' >
         <h1 className='text-8xl font-bold ml-32' >Popular {name}s of Naples</h1>
-        <div className='flex flex-col mt-10 gap-100'>
+        <div className='flex flex-col mt-10 gap-50'>
           {Object.values(foodItem.typeFood).map((food) => {
             return  <div className=' w-full relative ' key={food.name} >
                       <div className='flex items-center mx-auto  w-[90%]'>
-                        <img className='h-140 w-120 absolute ' src={food.pizzaPhoto} alt="" />
+                        
                       
-                        <div className='flex flex-col w-full pl-40 h-42'>
+                        <div className='flex flex-col relative w-full pl-40 h-42 '>
+                          <img className='h-140 w-120 absolute z-110 left-0 top-[-170px]' src={food.pizzaPhoto} alt="" />
                           <div className='flex w-full gap-30 items-center justify-between pl-80 bg-gray text-white' >
                             <h1 className='text-6xl'>{food.name}</h1>
                             <h2 className='text-6xl' >{food.price}$</h2>
                           </div>
 
-                          <ul className='text-3xl bg-gray-200 grid grid-rows-3 grid-flow-col pl-90 py-3 gap-x-3 gap-y-1 list-disc list-inside'>
+                          <ul className='relative text-3xl bg-gray-200 grid grid-rows-3 grid-flow-col pl-90 py-3  gap-y-1 list-disc list-inside'>
                             {food.ingredients.map((ing) => {
                               return <li key={ing} className='list-disc' >{ing}</li>
                             })}
-                          </ul>
+                            <button className="add-btn">Add to Get</button>                          </ul>
+                          
                         </div>
                         
                       </div>
