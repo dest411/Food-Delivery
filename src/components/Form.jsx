cfa7c0d4ab07dc10ff887f88a9fa0c2de3fc86b6
dest411 <vadimstrizhak652@gmail.com>
2025-12-02 17:48:57 +0200

    css

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,8 +8,8 @@ const Form = () => {
                 <p className='text-6xl text-center font-bold' >Share your opinion <br /> with us</p>
                 <div className='w-full h-35 my-8  px-10 flex justify-between'>
                     <div className='flex  flex-col justify-between   gap-2'>
-                        <input className='w-[380px]  bg-white rounded-2xl p-4 py-5 placeholder:text-2xl placeholder:text-gray-400' type="text"  placeholder='Your name' />
-                        <input type="email" placeholder='Your email' className='w-[380px] bg-white rounded-2xl p-4 py-5 placeholder:text-2xl placeholder:text-gray-400' /> 
+                        <input className='w-[380px]  bg-white rounded-2xl p-4 py-5 focus:text-2xl placeholder:text-2xl placeholder:text-gray-400' type="text"  placeholder='Your name' />
+                        <input type="email" placeholder='Your email' className='w-[380px] bg-white rounded-2xl p-4 py-5 placeholder:text-2xl focus:text-2xl placeholder:text-gray-400' /> 
                     </div>
                     <textarea  placeholder='Write something...'className='bg-white rounded-2xl w-[430px] h-full placeholder: text-2xl placeholder:p-2 placeholder:text-gray-400'></textarea>
                         
