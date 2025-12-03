bb210f82947aa3ab332e6eefc13f51ca7df19e7b
dest411 <vadimstrizhak652@gmail.com>
2025-12-03 20:11:59 +0200

    choise menu

--- src/components/ChoiseFood.jsx
+++ src/components/ChoiseFood.jsx
@@ -2,16 +2,27 @@ import React from 'react'
 import Appetizer from '../png/ChoiseFood/Appetizer.svg'
 import Dessert from '../png/ChoiseFood/Dessert.svg'
 import Beverage from '../png/ChoiseFood/Beverage.svg'
+import Wrapper from '../png/ChoiseFood/Wrapper.svg'
 
 const ChoiseFood = () => {
   return (
-    <div className='bg-blue-300 w-full h-auto'>
-        <div>
-            <div>
-
+    <div className='w-full h-auto flex justify-center'>
+        <div className=' flex justify-between w-[95%] h-auto max-w-[1500px]'>
+            <div className='relative cursor-pointer'>
+                <img className='' src={Appetizer} alt="" srcset="" />
+                <p className='text-7xl absolute left-15 bottom-40 z-50 text-white' >Appetizer</p>
+                <img className='absolute top-0' src={Wrapper} alt="" srcset="" />
             </div>
-            <div></div>
-            <div></div>
+            <div className='relative cursor-pointer'>
+                <img src={Dessert} alt="" srcset="" />
+                <p className='text-7xl absolute left-15 bottom-40 z-50 text-white'>Dessert</p>
+                <img className='absolute top-0' src={Wrapper} alt="" srcset="" />
+            </div>
+            <div className='relative cursor-pointer'>
+                <img src={Beverage} alt="" srcset="" />
+                <p className='text-7xl absolute left-15 bottom-40 z-50 text-white'>Beverage</p>
+                <img className='absolute top-0' src={Wrapper} alt="" srcset="" />
+            </div>
         </div>
     </div>
   )
