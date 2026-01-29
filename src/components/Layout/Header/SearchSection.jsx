31cbc756a018be849f0252d6f19aef707e44d4cc
dest411 <vadimstrizhak652@gmail.com>
2026-01-29 17:09:10 +0200

    npm install -D prettier prettier-plugin-tailwindcss

--- src/components/Layout/Header/SearchSection.jsx
+++ src/components/Layout/Header/SearchSection.jsx
@@ -59,49 +59,65 @@ const SearchSection = () => {
     };
 
     return (
-        <div ref={searchRef} className='flex w-auto *:items-center select-none ' >
-            <div className='relative w-20 sm:w-30 md:w-45 md:h-9 sm:h-8 h-7 flex items-center justify-between border border-black/10 rounded-2xl px-3 bg-black/5  '>
+        <div
+            ref={searchRef}
+            className="flex w-auto *:items-center select-none "
+        >
+            <div className="relative lg:w-50 w-20 sm:w-30 md:w-45 md:h-9 sm:h-8 h-7 flex items-center justify-between border border-black/10 rounded-2xl px-3 bg-black/5  ">
                 <input
-                    id='inputSearch' 
-                    className='w-15 h-4 sm:placeholder:text-[14px] md:placeholder:text-[20px] md:w-30 sm:w-18 placeholder:text-[8px] placeholder:text-gray-500
-                        flex items-center  text-xl leading-[50px] outline-none'                
-                    type="text" 
-                    placeholder='Search something...'
-                    value={searchItem} 
+                    id="inputSearch"
+                    className="w-15 lg: h-4 sm:placeholder:text-[14px] md:placeholder:text-[20px] md:w-30 sm:w-18 placeholder:text-[8px] placeholder:text-gray-500
+                        flex items-center  text-xl leading-[50px] outline-none"
+                    type="text"
+                    placeholder="Search something..."
+                    value={searchItem}
                     onChange={(e) => setSearchItem(e.target.value)}
                     onFocus={handleInputFocus}
                 />
                 {showSearch && searchItem.length > 0 && (
-                    <div className='absolute top-17 left-0 z-50 text-black rounded-3xl p-3 bg-white border border-gray-200 w-[330px] h-auto shadow-lg'>
-                        
+                    <div className="absolute top-17 left-0 z-50 text-black rounded-3xl p-3 bg-white border border-gray-200 w-[330px] h-auto shadow-lg">
                         {filteredFoods.length === 0 ? (
-                            <p className="text-2xl text-center text-gray-500">Nothing found</p>
+                            <p className="text-2xl text-center text-gray-500">
+                                Nothing found
+                            </p>
                         ) : (
-                            
                             filteredFoods.map((item, index) => (
-                                <div key={item.name + index} onClick={() => handleScrollToProduct(item)} className='flex items-center justify-between border-gray-200 border-b last:border-0'>
-                                    <p   className="flex gap-5 items-center text-2xl py-1   hover:text-orange-500 cursor-pointer transition">
-                                        <img className='w-15 h-15' src={item.typePhoto} alt="" />
+                                <div
+                                    key={item.name + index}
+                                    onClick={() => handleScrollToProduct(item)}
+                                    className="flex items-center justify-between border-gray-200 border-b last:border-0"
+                                >
+                                    <p className="flex gap-5 items-center text-2xl py-1   hover:text-orange-500 cursor-pointer transition">
+                                        <img
+                                            className="w-15 h-15"
+                                            src={item.typePhoto}
+                                            alt=""
+                                        />
                                         {item.name}
                                     </p>
-                                    <button onClick={(e) => {
-                                        e.stopPropagation();
-                                        addToBasket(item)}} className='add-btn2 w-15! h-8! text-[16px]! ' >Add to get</button>
-
+                                    <button
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            addToBasket(item);
+                                        }}
+                                        className="add-btn2 w-15! h-8! text-[16px]! "
+                                    >
+                                        Add to get
+                                    </button>
                                 </div>
-                                
-                                
                             ))
                         )}
-
                     </div>
                 )}
-                
-                <img className='absolute md:w-4 md:h-4 sm:w-3 sm:h-3 w-2 h-2 right-2' src={dandruff} alt="dandruff" />   
-            </div>
 
+                <img
+                    className="absolute md:w-4 md:h-4 sm:w-3 sm:h-3 w-2 h-2 right-2"
+                    src={dandruff}
+                    alt="dandruff"
+                />
+            </div>
         </div>
-    )
+    );
 }
 
 export default SearchSection
