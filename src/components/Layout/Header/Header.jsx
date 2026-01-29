e9b1d4b0735360f13a36f7426c466f4160c1f432
dest411 <vadimstrizhak652@gmail.com>
2026-01-29 23:51:12 +0200

    welcome sec

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     };
     return (
         <header className="sticky top-0 z-200 flex w-full justify-center border-b border-gray-100 bg-white shadow-xl backdrop-blur-sm">
-            <div className="not-[]:top-0 relative z-140 flex h-9 w-[95%] max-w-[1500px] items-center justify-between sm:h-12 md:h-14 lg:h-20 xl:h-23">
+            <div className="not-[]:top-0 relative z-140 flex h-7 w-[95%] max-w-[1500px] items-center justify-between sm:h-12 md:h-14 lg:h-20 xl:h-23">
                 <div className="flex w-6/10 items-center gap-10 md:gap-20 lg:gap-25 2xl:gap-35">
                     <p className="from-gray w-auto bg-linear-to-r to-[#C0C0C2] bg-clip-text text-lg font-bold text-transparent sm:text-3xl md:text-5xl lg:text-6xl 2xl:text-8xl">
                         Naples
--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -1,32 +1,41 @@
-import React from 'react'
-import background from '../../png/bg.svg'
+import React from 'react';
+import background from '../../png/bg.svg';
 
 const Hero = React.memo(() => {
-  console.log("hero render");
-  const scrollToMenu = () => {
-    const targetElement = document.getElementById('menu-choise');
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
-  }
+    console.log('hero render');
+    const scrollToMenu = () => {
+        const targetElement = document.getElementById('menu-choise');
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+            });
+        }
+    };
 
-  return (
-    <div 
-        style={{ backgroundImage:`linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${background})`,
-        
-    
-    }}
-        className='w-full h-[700px]  flex flex-col justify-center items-center text-center bg-cover bg-center  text-white' 
-    >
-        <h1 className='text-9xl' >The fastest delivery in <br /> your city</h1>
-        <p className='text-7xl' > Visit our restaurant to taste our italian cuisine or <br /> simply order it from our website.</p>
-        <button onClick={scrollToMenu} className='text-5xl border py-3 px-8 rounded-3xl cursor-pointer 
-        transition duration-300 hover:bg-white/15 transform hover:scale-110
-        active:scale-95 active:bg-white/25' >Order now</button>
-    </div>
-  )
-})
+    return (
+        <div
+            style={{
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${background})`,
+            }}
+            className="flex h-[150px] w-full flex-col items-center justify-center bg-cover bg-center py-20 text-center text-white sm:h-[300px] md:h-[400px] lg:h-[600px] xl:h-[800px]"
+        >
+            <div className="flex flex-col items-center gap-3 md:gap-5 lg:gap-7 xl:gap-10">
+                <h1 className="text-xl leading-4 sm:text-4xl sm:leading-6 md:text-6xl md:leading-9 lg:text-8xl lg:leading-15 xl:text-9xl xl:leading-20">
+                    The fastest delivery in <br /> your city
+                </h1>
+                <p className="text-[10px] leading-2 sm:text-sm sm:leading-4 md:text-xl md:leading-5 lg:text-3xl lg:leading-7 xl:text-4xl xl:leading-9">
+                    Visit our restaurant to taste our italian cuisine or <br />
+                    simply order it from our website.
+                </p>
+                <button
+                    onClick={scrollToMenu}
+                    className="transform cursor-pointer rounded-lg border px-5 py-1 text-[10px] transition duration-300 hover:scale-110 hover:bg-white/15 active:scale-95 active:bg-white/25 sm:text-sm md:rounded-2xl md:text-2xl lg:text-4xl xl:rounded-4xl xl:px-7 xl:py-3 xl:text-6xl"
+                >
+                    Order now
+                </button>
+            </div>
+        </div>
+    );
+});
 
-export default Hero
+export default Hero;
