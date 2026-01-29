0c13883de41092c7fc05570f6f818ca2514aa7c9
dest411 <vadimstrizhak652@gmail.com>
2026-01-29 16:11:21 +0200

    val

--- src/components/Cart/Form.jsx
+++ src/components/Cart/Form.jsx
@@ -33,7 +33,6 @@ const Form = memo(() => {
     return (
         <div className="w-full h-auto my-50 flex justify-center items-center">
             <div className="w-full max-w-[920px]">
-                
                 {/* onSubmit */}
                 <form
                     onSubmit={handleSubmit(onSubmit)}
@@ -46,26 +45,22 @@ const Form = memo(() => {
                     <div className="w-full h-35 my-8 px-10 flex justify-between">
                         <div className="flex flex-col justify-between gap-2">
                             <input
-                                name="name"
-                                value={formData.name}
-                                onChange={handleInputChange}
+                                {...register('name')}
                                 className="w-[380px] focus:outline-none focus:border-0 bg-white rounded-2xl p-4 py-5 placeholder:text-2xl placeholder:text-gray-400"
                                 type="text"
                                 placeholder="Your name"
                             />
                             <input
-                                name="email"
-                                value={formData.email}
-                                onChange={handleInputChange}
+                                {...register('email')}
                                 type="email"
                                 placeholder="Your email"
                                 className="w-[380px] focus:outline-none focus:border-0 bg-white rounded-2xl p-4 py-5 placeholder:text-2xl placeholder:text-gray-400"
                             />
                         </div>
                         <textarea
-                            name="message"
-                            value={formData.message}
-                            onChange={handleInputChange}
+                            {...register('message'), {
+                                required: 'Please, write something'
+                            }}
                             placeholder="Write something..."
                             className="resize-none focus:outline-none focus:border-0 bg-white p-2 rounded-2xl w-[430px] h-full placeholder:text-2xl placeholder:text-gray-400"
                             required
@@ -74,12 +69,12 @@ const Form = memo(() => {
 
                     <button
                         type="submit"
-                        disabled={isSending}
+                        disabled={isSubmitting}
                         className={`border-red-500 text-red-700 text-3xl font-bold border rounded-3xl w-[140px] h-[50px] cursor-pointer transition-transform duration-200 
                         ${isSending ? 'opacity-50 cursor-not-allowed' : 'hover:scale-110'}
                     `}
                     >
-                        {isSending ? '...' : 'Send'}
+                        {isSubmitting ? '...' : 'Send'}
                     </button>
                 </form>
             </div>
