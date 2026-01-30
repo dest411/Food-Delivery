496766e361314192d4b44904d16dc46fdf110880
dest411 <vadimstrizhak652@gmail.com>
2026-01-30 19:42:09 +0200

    feat: welcome section responsive/adaptive design completed (lg, next xl, 2xl)

--- src/components/Sections/WelcomeSection.jsx
+++ src/components/Sections/WelcomeSection.jsx
@@ -1,35 +1,50 @@
-import React from 'react'
+import React from 'react';
 
 const WelcomeSection = () => {
-  return (
-    <div className='text-4xl relative w-[95%] h-auto flex flex-col items-center select-none' >
-        <h1 className='font-[Caveat] text-9xl opacity-10 absolute mt-14'>Welcome to Naples</h1>
-        <h2 className='text-8xl mt-30 opacity-75' >Welcome to Naples</h2>
-        <p className='text-center opacity-60  text-5xl' >We started working in 2007 and in 2012 we were able to <br /> open our second branch and now we have several <br /> branches all over the country.</p>
-        <div 
-            className="w-[545px] h-11 bg-white flex items-center justify-center mt-10"
-            aria-label="Zigzag Pattern Container"
-            >
-            <svg 
-                className="w-full h-full" 
-                viewBox="0 0 545 44" 
-                preserveAspectRatio="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                
-                <polyline 
-                points="0,4 54.5,40 109,4 163.5,40 218,4 272.5,40 327,4 381.5,40 436,4 490.5,40 545,4"
-                style={{
-                    fill: 'none',
-                    stroke: '#E5EEED',
-                    strokeWidth: 8,
-                    strokeLinecap: 'round'
-                }}
-                />
-            </svg>
-        </div>
-    </div>
-  )
-}
+    return (
+        <div className="flex h-auto w-[95%] flex-col items-center justify-end py-10 select-none sm:py-15 md:py-20 lg:py-25">
+            <div className="relative flex w-full justify-center">
+                <h2 className="text-2xl opacity-75 sm:text-4xl md:text-5xl lg:text-6xl">
+                    Welcome to Naples
+                </h2>{' '}
+                {/* text-2xl  */}
+                <h1 className="absolute bottom-2 font-[Caveat] text-4xl opacity-10 sm:bottom-3 sm:text-6xl md:bottom-4 md:text-7xl lg:bottom-4.5 lg:text-8xl">
+                    {' '}
+                    {/* text-4xl  bottom-2 */}
+                    Welcome to Naples
+                </h1>
+            </div>
 
-export default WelcomeSection
+            <p className="text-center text-sm leading-3 opacity-60 sm:text-lg sm:leading-4 md:text-xl md:leading-5 lg:text-2xl lg:leading-6">
+                {' '}
+                {/* text-sm leading-3 */}
+                We started working in 2007 and in 2012 we were able to <br />{' '}
+                open our second branch and now we have several <br /> branches
+                all over the country.
+            </p>
+            <div
+                className="mt-5 flex h-3 w-35 items-center justify-center bg-white sm:h-5 sm:w-50 md:mt-7 md:h-7 md:w-65 lg:mt-10 lg:h-9 lg:w-85" //h-3 w-[145px]
+                aria-label="Zigzag Pattern Container"
+            >
+                <svg
+                    className="h-full w-full"
+                    viewBox="0 0 545 44"
+                    preserveAspectRatio="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <polyline
+                        points="0,4 54.5,40 109,4 163.5,40 218,4 272.5,40 327,4 381.5,40 436,4 490.5,40 545,4"
+                        style={{
+                            fill: 'none',
+                            stroke: '#E5EEED',
+                            strokeWidth: 8,
+                            strokeLinecap: 'round',
+                        }}
+                    />
+                </svg>
+            </div>
+        </div>
+    );
+};
+
+export default WelcomeSection;
