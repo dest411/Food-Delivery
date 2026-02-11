18c2780cecb58f5ff13a93ea6a806f80c3db399e
dest411 <vadimstrizhak652@gmail.com>
2026-02-11 20:38:49 +0200

    menu lg

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -11,7 +11,7 @@ const Menu = () => {
     return (
         <div id="menu-choise" className="my-5 h-auto w-[95%] max-w-[1500px]">
             <div className="flex h-auto w-full justify-between">
-                <div className="absolute left-0 flex w-1/6 items-center justify-center bg-red-700 px-6 py-2 text-center text-xl font-semibold text-white [clip-path:polygon(0%_0%,87%_0%,100%_50%,87%_100%,0%_100%)] sm:px-8 sm:py-6 sm:text-2xl md:px-15 md:text-4xl">
+                <div className="absolute left-0 flex w-1/6 items-center justify-center bg-red-700 px-6 py-2 text-center text-xl font-semibold text-white [clip-path:polygon(0%_0%,87%_0%,100%_50%,87%_100%,0%_100%)] sm:px-8 sm:py-6 sm:text-2xl md:px-15 md:text-4xl lg:px-25 lg:py-8 lg:text-6xl">
                     Menu
                 </div>
 
@@ -22,16 +22,16 @@ const Menu = () => {
                         const isActive = food.name === activeMenu;
                         return (
                             <div
-                                className={`box-shadow: -1px 0px 80px -3px rgba(0,0,0,0.75) flex w-22 cursor-pointer items-center gap-1 rounded-xl border px-2 py-2 shadow-[0px_0px_20px_-3px_rgba(0,0,0,0.40)] transition-colors duration-300 sm:w-33 sm:gap-2 sm:rounded-2xl sm:px-4 sm:py-5 md:w-45 md:gap-4 ${isActive ? 'border-amber-600' : 'border-black/7'}`}
+                                className={`box-shadow: -1px 0px 80px -3px rgba(0,0,0,0.75) flex w-22 cursor-pointer items-center gap-1 rounded-xl border px-2 py-2 shadow-[0px_0px_20px_-3px_rgba(0,0,0,0.40)] transition-colors duration-300 sm:w-33 sm:gap-2 sm:rounded-2xl sm:px-4 sm:py-5 md:w-45 md:gap-4 lg:w-60 ${isActive ? 'border-amber-600' : 'border-black/7'}`}
                                 key={food.id}
                                 onClick={() => setActiveMenu(food.name)}
                             >
                                 <img
-                                    className="h-8 sm:h-10 md:h-15"
+                                    className="h-8 sm:h-10 md:h-15 lg:h-20"
                                     src={food.photo}
                                     alt=""
                                 />
-                                <p className="text-[15px] sm:text-[20px] md:text-3xl">
+                                <p className="text-[15px] sm:text-[20px] md:text-3xl lg:text-5xl">
                                     {food.name}
                                 </p>
                             </div>
