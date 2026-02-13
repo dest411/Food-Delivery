e208f1e26e573f1815b494dd8bd954ba16da04d5
dest411 <vadimstrizhak652@gmail.com>
2026-02-13 12:26:46 +0200

    default h, w menu food

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,14 +1,14 @@
-import pizza from './png/pizza.svg'
-import pasta from './png/pasta.svg'
-import burger from './png/burger.svg'
+import pizza from './png/pizza.svg';
+import pasta from './png/pasta.svg';
+import burger from './png/burger.svg';
 
-import PepperoniPizza from './png/PizzaPhoto/PepperoniPizza.svg'
-import VegetablesPizza from './png/PizzaPhoto/VegetablesPizza.svg'
-import MargheritaPizza from './png/PizzaPhoto/MargheritaPizza.svg'
+import PepperoniPizza from './png/PizzaPhoto/PepperoniPizza.svg';
+import VegetablesPizza from './png/PizzaPhoto/VegetablesPizza.svg';
+import MargheritaPizza from './png/PizzaPhoto/MargheritaPizza.svg';
 
-import doubleBeefBurger from './png/BurgersPhoto/doubleBeefBurger.png'
-import chickenBurger from './png/BurgersPhoto/chickenBurger.png'
-import cheeseBurger from './png/BurgersPhoto/cheeseBurger.png'
+import doubleBeefBurger from './png/BurgersPhoto/doubleBeefBurger.png';
+import chickenBurger from './png/BurgersPhoto/chickenBurger.png';
+import cheeseBurger from './png/BurgersPhoto/cheeseBurger.png';
 
 const Foods = [
     {
@@ -17,27 +17,47 @@ const Foods = [
         photo: pizza,
         typeFood: {
             Pepperoni: {
-                typePhoto: PepperoniPizza, 
-                imgStyle: "w-[480px] h-[560px]",
+                typePhoto: PepperoniPizza,
+                // imgStyle: 'w-[100px] h-[100px]',
                 name: 'Pepperoni pizza',
                 price: '15,00',
-                ingredients: ['Pepperoni sausage', 'Cheese pizza', 'Red bell pepper', 'Ketchup', 'Thyme', 'Tomato']
+                ingredients: [
+                    'Pepperoni sausage',
+                    'Cheese pizza',
+                    'Red bell pepper',
+                    'Ketchup',
+                    'Thyme',
+                    'Tomato',
+                ],
             },
             Vegetables: {
                 typePhoto: VegetablesPizza,
-                imgStyle: "w-[480px] h-[560px]", 
+                // imgStyle: 'w-[100px] h-[100px]',
                 name: 'Vegetables pizza',
                 price: '12,00',
-                ingredients: ['Mushroom', 'Corn', 'Black olive', 'Tomato', 'Peas']
+                ingredients: [
+                    'Mushroom',
+                    'Corn',
+                    'Black olive',
+                    'Tomato',
+                    'Peas',
+                ],
             },
             Margherita: {
                 typePhoto: MargheritaPizza,
-                imgStyle: "w-[480px] h-[560px]",
+                // imgStyle: 'w-[480px] h-[560px]',
                 name: 'Margherita pizza',
                 price: '13,90',
-                ingredients: ['Basil', 'Cheese pizza', 'Black pepper', 'Garlic', 'Thyme', 'Tomato']
-            }
-        }
+                ingredients: [
+                    'Basil',
+                    'Cheese pizza',
+                    'Black pepper',
+                    'Garlic',
+                    'Thyme',
+                    'Tomato',
+                ],
+            },
+        },
     },
     {
         id: 2,
@@ -46,26 +66,45 @@ const Foods = [
         typeFood: {
             Cheese: {
                 typePhoto: cheeseBurger,
-                imgStyle: "w-[400px] h-[280px] top-[-50px]!", 
+                imgStyle: 'w-[400px] h-[280px] top-[-50px]!',
                 name: 'Cheese Burger',
                 price: '6,00',
-                ingredients: ['Beef patty', 'Cheddar cheese', 'Pickles', 'Onion', 'Mustard', 'Ketchup']
+                ingredients: [
+                    'Beef patty',
+                    'Cheddar cheese',
+                    'Pickles',
+                    'Onion',
+                    'Mustard',
+                    'Ketchup',
+                ],
             },
             Chicken: {
                 typePhoto: chickenBurger,
-                imgStyle: "w-[500] h-[304px] top-[-50px]!", 
+                imgStyle: 'w-[500] h-[304px] top-[-50px]!',
                 name: 'Chicken Burger',
                 price: '7,00',
-                ingredients: ['Chicken fillet', 'Lettuce', 'Tomato', 'Mayonnaise', 'Sesame bun']
+                ingredients: [
+                    'Chicken fillet',
+                    'Lettuce',
+                    'Tomato',
+                    'Mayonnaise',
+                    'Sesame bun',
+                ],
             },
             Double: {
                 typePhoto: doubleBeefBurger,
-                imgStyle: "w-[400px] h-[400px] top-[-120px]!",
+                imgStyle: 'w-[400px] h-[400px] top-[-120px]!',
                 name: 'Double Beef Burger',
                 price: '8,50',
-                ingredients: ['Double beef patty', 'Bacon', 'BBQ sauce', 'Caramelized onion', 'Cheese']
-            }
-        }
+                ingredients: [
+                    'Double beef patty',
+                    'Bacon',
+                    'BBQ sauce',
+                    'Caramelized onion',
+                    'Cheese',
+                ],
+            },
+        },
     },
     {
         id: 3,
@@ -74,27 +113,46 @@ const Foods = [
         typeFood: {
             Carbonara: {
                 typePhoto: null,
-                imgStyle: "w-[480px] h-[560px]", 
+                imgStyle: 'w-[480px] h-[560px]',
                 name: 'Pasta Carbonara',
                 price: '12,00',
-                ingredients: ['Spaghetti', 'Bacon', 'Egg yolk', 'Parmesan cheese', 'Black pepper']
+                ingredients: [
+                    'Spaghetti',
+                    'Bacon',
+                    'Egg yolk',
+                    'Parmesan cheese',
+                    'Black pepper',
+                ],
             },
             Bolognese: {
-                typePhoto: null, 
+                typePhoto: null,
                 name: 'Pasta Bolognese',
-                imgStyle: "w-[480px] h-[560px]",
+                imgStyle: 'w-[480px] h-[560px]',
                 price: '18,00',
-                ingredients: ['Tagliatelle', 'Minced beef', 'Tomato sauce', 'Carrot', 'Celery', 'Red wine']
+                ingredients: [
+                    'Tagliatelle',
+                    'Minced beef',
+                    'Tomato sauce',
+                    'Carrot',
+                    'Celery',
+                    'Red wine',
+                ],
             },
             Pesto: {
                 typePhoto: null,
-                imgStyle: "w-[480px] h-[560px]",
+                imgStyle: 'w-[480px] h-[560px]',
                 name: 'Pasta Pesto',
                 price: '16,00',
-                ingredients: ['Penne', 'Basil pesto', 'Pine nuts', 'Olive oil', 'Parmesan cheese']
-            }
-        }
-    }
-]
+                ingredients: [
+                    'Penne',
+                    'Basil pesto',
+                    'Pine nuts',
+                    'Olive oil',
+                    'Parmesan cheese',
+                ],
+            },
+        },
+    },
+];
 
-export default Foods
+export default Foods;
--- src/components/Menu/MenuFood.jsx
+++ src/components/Menu/MenuFood.jsx
@@ -1,57 +1,85 @@
-import React from 'react'
+import React from 'react';
 import Foods from '../../Food';
 import { useStore } from '../../store/Store';
 
 const MenuFood = () => {
+    const activeMenu = useStore((state) => state.activeMenu);
+    const addToBasket = useStore((state) => state.addToBasket);
 
-  const activeMenu = useStore((state) => state.activeMenu);
-  const addToBasket = useStore((state) => state.addToBasket);
-  
-  console.log("render menu food");  
+    console.log('render menu food');
 
-  const foodItem = Foods.find(item => item.name === activeMenu);
-  if (!foodItem) {
-      
-    return <p className='text-4xl uppercase' >Menu is not found</p>;
-  }
-   
-  const name = foodItem.name;
+    const foodItem = Foods.find((item) => item.name === activeMenu);
+    if (!foodItem) {
+        return <p className="text-4xl uppercase">Menu is not found</p>;
+    }
 
-  return (
-    <div className='w-full max-w-[1500px] mx-auto ' >
-        <h1 className='text-8xl font-bold ml-32' >Popular {name}s of Naples</h1>
-        <div className='flex flex-col mt-10 gap-50'>
-          {Object.values(foodItem.typeFood).map((food) => {
-            return  <div id={food.name.replace(/\s/g, '')} className=' w-full relative ' key={food.name} >
-                      <div className='flex items-center mx-auto  w-[90%]'>
-                        
-                        <div className='flex flex-col relative w-full pl-40 h-42 '>
-                          {food.typePhoto == null ? 
-                            <div className='skeleton' >PHOTO {food.name} <br /> (NOT LOADED)</div>
-                            :
-                            <img className={`select-none absolute z-110 left-0 top-[-170px] ${food.imgStyle || 'w-120 h-140 top-[-170px]'}`} src={food.typePhoto} alt="" />}
-                          
-                          <div className='flex w-full gap-30 items-center justify-between pl-80 pr-3 bg-gray text-white' >
-                            <h1 className='text-6xl'>{food.name}</h1>
-                            <h2 className='text-6xl' >{food.price}$</h2>
-                          </div>
+    const name = foodItem.name;
 
-                          <ul className='relative text-3xl bg-gray-200 grid grid-rows-3 grid-flow-col pl-90 py-3  gap-y-1 list-disc list-inside'>
-                            {food.ingredients.map((ing) => {
-                              return <li key={ing} className='list-disc' >{ing}</li>
-                            })}
-                            <button onClick={() => addToBasket(food)} className="add-btn">Add to Get</button>                          </ul>
-                          
+    return (
+        <div className="mx-auto w-full max-w-[1500px]">
+            <h1 className="mx-auto w-[90%] text-3xl font-bold sm:text-5xl">
+                Popular {name}s of Naples
+            </h1>
+            <div className="mt-10 flex flex-col gap-10 sm:gap-20">
+                {Object.values(foodItem.typeFood).map((food) => {
+                    return (
+                        <div
+                            id={food.name.replace(/\s/g, '')}
+                            className="relative w-full"
+                            key={food.name}
+                        >
+                            <div className="mx-auto flex w-[90%] items-center">
+                                <div className="relative flex h-auto w-full flex-col">
+                                    {food.typePhoto == null ? (
+                                        <div className="skeleton">
+                                            PHOTO {food.name} <br /> (NOT
+                                            LOADED)
+                                        </div>
+                                    ) : (
+                                        <img
+                                            className={`absolute top-[-30%] left-[-15px] z-110 select-none sm:top-[-43%] ${'h-40 w-40 sm:h-60 sm:w-60 ' || 'top-[-170px] h-5 w-5'}`}
+                                            src={food.typePhoto}
+                                            alt=""
+                                        />
+                                    )}
+
+                                    <div className="bg-gray flex w-full items-center justify-between pr-3 pl-[40%] text-white">
+                                        <h1 className="text-xl sm:text-3xl">
+                                            {food.name}
+                                        </h1>
+                                        <h2 className="text-xl sm:text-3xl">
+                                            {food.price}$
+                                        </h2>
+                                    </div>
+
+                                    <div className="flex h-18 w-full sm:h-24">
+                                        <div className="h-full w-[40%] bg-gray-100"></div>
+                                        <ul className="relative grid h-full w-[60%] list-inside list-disc grid-flow-col grid-rows-4 bg-gray-100 p-2 text-[13px] sm:text-[18px]">
+                                            {food.ingredients.map((ing) => {
+                                                return (
+                                                    <li key={ing} className="">
+                                                        {ing}
+                                                    </li>
+                                                );
+                                            })}
+                                            <button
+                                                onClick={() =>
+                                                    addToBasket(food)
+                                                }
+                                                className="absolute right-2 bottom-2 flex h-5 w-20 cursor-pointer items-center justify-center rounded-xl border-none bg-[#ba1c1c] text-white shadow-[0px_4px_0px_#7f1d1d] transition-all duration-100 ease-linear active:translate-y-[2px] active:shadow-none"
+                                            >
+                                                Add to Get
+                                            </button>{' '}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                    </div>
-          })}
+                    );
+                })}
+            </div>
         </div>
-        
-        
-       
-    </div>
-  )
-}
+    );
+};
 
 export default MenuFood;
