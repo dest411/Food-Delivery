a25a4e9616662ca29e7f8d196a1ae41090519671
dest411 <vadimstrizhak652@gmail.com>
2026-02-15 00:47:58 +0200

    tailwindconfig

--- src/components/Cart/ModalBasket.jsx
+++ src/components/Cart/ModalBasket.jsx
@@ -1,82 +1,135 @@
-import React, { memo } from 'react'
-import garbage from '../../png/garbage.svg'
+import React, { memo } from 'react';
+import garbage from '../../png/garbage.svg';
 import { useStore } from '../../store/Store';
 
 const ModalBasket = () => {
-
     console.log('render modalBasket');
-    
+
     const basket = useStore((state) => state.basket);
     const isModalBasketOpen = useStore((state) => state.isModalBasketOpen);
     const addToBasket = useStore((state) => state.addToBasket);
     const removeFromBasket = useStore((state) => state.removeFromBasket);
     const removeCompletely = useStore((state) => state.removeCompletely);
     const proceedToCheckout = useStore((state) => state.openCheckout);
-    
+
     const totalPrice = useStore((state) => state.getTotalPrice());
 
     return (
-    <div className='fixed top-0 left-2 w-full h-full z-202 flex justify-center pointer-events-none'>
-        
-        <div className='w-full max-w-[1500px] relative h-full'>
-            
-            <div className={` 
-                absolute top-20 pointer-events-auto 
-                bg-white
-                h-auto max-h-[65vh] border p-5 w-150 min-h-50'
-                flex flex-col
-                transition-all duration-300 ease-in-out 
-                ${isModalBasketOpen ? 'right-0' : '-right-[3000px]'}`}>  {/* <-- MAIN/ */}
-                
-                {basket.length == 0 ? <p className='text-4xl flex items-center justify-center h-full'>The basket is empty</p> :
+        <div
+            className={`pointer-events-none fixed top-0 z-150 flex h-full w-full justify-center ${isModalBasketOpen && 'bg-black/40'}`}
+        >
+            <div className="flex h-screen w-full max-w-[1500px] items-center justify-center">
+                <div
+                    className={`pointer-events-auto flex h-auto w-8/10 flex-col rounded-2xl border-none ${
+                        isModalBasketOpen
+                            ? 'visible scale-100 opacity-100'
+                            : 'invisible scale-90 opacity-0'
+                    } bg-white p-5 transition-all duration-300 ease-in-out ${isModalBasketOpen ? 'right-0' : '-right-[3000px]'}`}
+                >
+                    {' '}
+                    {/* <-- MAIN/ */}
+                    {basket.length == 0 ? (
+                        <p className="flex h-full items-center justify-center text-4xl">
+                            The basket is empty
+                        </p>
+                    ) : (
+                        <div className="flex h-full flex-col overflow-hidden">
+                            {' '}
+                            {/* MAIN */}
+                            {/* LIST FOOD ID MODAL BASKET */}
+                            <div className="custom-scrollbar flex flex-1 flex-col gap-2 overflow-y-auto">
+                                {basket.map((food) => {
+                                    return (
+                                        <div
+                                            key={food.name}
+                                            className="flex h-[50px] min-h-20 w-full shrink-0 items-center justify-between gap-2 rounded"
+                                        >
+                                            <div className="w-60b flex w-1/2 items-center gap-3">
+                                                <img
+                                                    className="h-20 w-20"
+                                                    src={food.typePhoto}
+                                                    alt="food photo"
+                                                />
+                                                <p
+                                                    key={food.name}
+                                                    className="text-3xl"
+                                                >
+                                                    {food.name}
+                                                </p>
+                                            </div>
 
-                <div className='flex flex-col h-full overflow-hidden ' > {/* MAIN */}
-                    
-                    {/* LIST FOOD ID MODAL BASKET */}
-                    <div className=' overflow-y-auto custom-scrollbar flex-1 flex flex-col gap-2 ' >
+                                            <div className="flex w-[15%] min-w-20 cursor-pointer items-center gap-2 text-2xl select-none">
+                                                <p
+                                                    onClick={() =>
+                                                        removeFromBasket(food)
+                                                    }
+                                                >
+                                                    -
+                                                </p>
+                                                <p className="flex h-6 w-auto min-w-8 items-center justify-center rounded border text-2xl">
+                                                    {food.count}
+                                                </p>
+                                                <p
+                                                    onClick={() =>
+                                                        addToBasket(food)
+                                                    }
+                                                >
+                                                    +
+                                                </p>
+                                            </div>
 
-                        {basket.map((food) => {
-                            return <div key={food.name} className='flex w-full h-[50px] min-h-20 items-center justify-between gap-2 rounded shrink-0' >                           
-                                <div className=' flex w-1/2 gap-3  items-center w-60b '>
-                                    <img className='w-20 h-20' src={food.typePhoto}  alt="food photo"/>
-                                    <p key={food.name} className='text-3xl' >{food.name}</p>
+                                            <img
+                                                onClick={() =>
+                                                    removeCompletely(food)
+                                                }
+                                                className="h-3 w-3 cursor-pointer"
+                                                src={garbage}
+                                                alt=""
+                                            />
+                                            <p
+                                                key={food.name}
+                                                className="w-[20%] text-2xl"
+                                            >
+                                                Price:{' '}
+                                                {(
+                                                    parseFloat(
+                                                        food.price.replace(
+                                                            ',',
+                                                            '.',
+                                                        ),
+                                                    ) * food.count
+                                                ).toFixed(2)}
+                                                $
+                                            </p>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            {/* FOOTER MODAL BASKET */}
+                            <div className="mt-2 flex shrink-0 flex-col gap-5 border-t border-black/20 py-2 pt-4">
+                                <div className="flex justify-between">
+                                    <p className="text-right text-4xl font-bold">
+                                        Total to order:
+                                    </p>
+                                    <p className="text-right text-4xl font-bold text-orange-400">
+                                        {totalPrice}$
+                                    </p>
                                 </div>
-                                
-                                <div className='flex text-2xl gap-2 items-center cursor-pointer w-[15%]  min-w-20 select-none' >
-                                    <p onClick={()=>removeFromBasket(food)} >-</p>
-                                    <p className='text-2xl border w-auto min-w-8 h-6 flex items-center justify-center rounded' >{food.count}</p>
-                                    <p onClick={()=> addToBasket(food)} >+</p>
-                                </div>
 
-                                <img onClick={() => removeCompletely(food)} className='w-3 h-3 cursor-pointer ' src={garbage} alt="" />
-                                <p key={food.name} className='text-2xl w-[20%]'>
-                                    Price: {(parseFloat(food.price.replace(',', '.')) * food.count).toFixed(2)}$
-                                </p>                            
-                            </div> 
-                        })}
-                    </div>
-
-                    {/* FOOTER MODAL BASKET */}
-                    <div className='mt-2 pt-4 flex flex-col gap-5 py-2 border-t border-black/20 shrink-0 '>
-                        <div className='flex justify-between' >
-                            <p className='text-4xl font-bold text-right' >Total to order:</p>
-                            <p className='text-4xl text-orange-400 font-bold text-right'>{totalPrice}$</p> 
+                                <button
+                                    onClick={proceedToCheckout}
+                                    className="add-btn2 text-2xl"
+                                >
+                                    Proceed to checkout
+                                </button>
+                            </div>
                         </div>
-                        
-                        <button onClick={proceedToCheckout} className="add-btn2 text-2xl">Proceed to checkout</button>
-
-                    </div>
-
-                    
-
+                    )}{' '}
+                    {/* <-- MAIN */}
                 </div>
-                
-                } {/* <-- MAIN */}
-                
             </div>
         </div>
-    </div>
-    )
-}
+    );
+};
 
-export default ModalBasket
+export default ModalBasket;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
   theme: {
+    screens: {
+      'xs': '450px',
+      ...defaultTheme.screens,
+    },
+
     extend: {
       fontFamily: {
         caveat: ['"Caveat"', 'cursive'],
       },
     },
   },
+  plugins: [],
 }
